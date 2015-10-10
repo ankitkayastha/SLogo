a791934d36ca276ff18fc81d025154e1112a912f
Ankit Kayastha <ankit.kayastha@duke.edu>
2015-10-10 16:04:51 -0400

    Created Left Panel

---
+++ src/LeftPane.java
@@ -0,0 +1,35 @@
+import javafx.scene.Group;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+
+public class LeftPane {
+	private Group root;
+	
+	public LeftPane() {
+		root = makeScrollablePanes();
+	}
+	
+	public Group makeScrollablePanes() {
+		ScrollPane pane = new ScrollPane();
+		ScrollPane paneTwo = new ScrollPane();
+		Text textOne = new Text();
+		textOne.setText("User Defined Functions");
+		Text textTwo = new Text();
+		textTwo.setText("Variables");
+		pane.setPrefSize(150, 675/2);
+		paneTwo.setPrefSize(150, 675/2);
+		paneTwo.setTranslateY(675/2);
+		textOne.setTranslateY(20);
+		textTwo.setTranslateY(675/2 + 20);
+		textOne.setTextAlignment(TextAlignment.JUSTIFY);
+		textTwo.setTextAlignment(TextAlignment.JUSTIFY);
+		Group root = new Group();
+		root.getChildren().add(pane);
+		root.getChildren().add(paneTwo);
+		root.getChildren().add(textOne);
+		root.getChildren().add(textTwo);
+		return root;
+	}
+}
---
+++ src/TurtleView.java
@@ -0,0 +1,20 @@
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
+
+public class TurtleView {
+	private Scene myScene;
+	public TurtleView() {
+		BorderPane myPane = new BorderPane();
+		LeftPane left = new LeftPane();
+		//myPane.setCenter(value);
+		myPane.setLeft(left.makeScrollablePanes());
+		//myPane.setRight(value);
+		//myPane.setTop(value);
+		//myPane.setBottom(value);
+		myScene = new Scene(myPane, 900, 675);
+	}
+	
+	public Scene getScene() {
+		return myScene;
+	}
+}
