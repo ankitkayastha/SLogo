442508bad1c11dc6a2e1b4a619e97bb4e55080b3
Nicky Groszewski <groszewn@gmail.com>
2015-10-25 14:44:47 -0400

    helper functions to add to menu

--- src/UserInterface/TopPane/MenuHandler.java
+++ src/UserInterface/TopPane/MenuHandler.java
@@ -6,9 +6,11 @@ import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Hyperlink;
@@ -42,82 +44,82 @@ public class MenuHandler implements IFront {
 		return this.root;
 	}
 	
+	private Rectangle[] makeColorNodes(String[] options) {
+		Rectangle[] rects = new Rectangle[options.length];
+		for (int i = 0; i < rects.length; i++) {
+			Rectangle rect = new Rectangle(50,10);
+			rect.setFill(Color.web(options[i]));
+			rects[i] = rect;
+		}
+		return rects;
+	}
+	
+	private Rectangle[] makeImageNodes(String[] options, int width, int length) {
+		Rectangle[] rects = new Rectangle[options.length];
+		for (int i = 0; i < rects.length; i++) {
+			Rectangle rect = new Rectangle(width,length);
+			rect.setFill(new ImagePattern(new Image(getClass().getClassLoader().getResourceAsStream(options[i]))));
+			rects[i] = rect;
+		}
+		return rects;
+	}
+	
 	public void makeMenuBar(TopPane c, ReceiveFromFront rs) {
 		MenuBar menuBar = new MenuBar();
 		root = new Group();
-//		HBox box = new HBox();
-//		Menu backgroundColor = new Menu(r.getString("backgroundTitle"));
-//		
-//		ListView<String> list = new ListView<String>();
-//		list.setPrefSize(120, 200);
-//		ListView<Integer> indeces = new ListView<Integer>();
-//		indeces.setPrefSize(50,  200);
-//		ObservableList<Integer> ind = FXCollections.observableArrayList();
-//		for (int i = 0; i < 10; i++) {
-//			ind.add(i);
-//		}
-//		ObservableList<String> obs = FXCollections.observableArrayList("salmon", "green", "blue", "red", 
-//				"chocolate", "yellow", "pink", "purple", "orange");
-//		list.setItems(obs);
-//		indeces.setItems(ind);
-//		list.setCellFactory(new Callback<ListView<String>, ListCell<String>>() {
-//			@Override
-//			public ListCell<String> call (ListView<String> list) {
-//				return new ColorRectCell();
-//			}
-//		}
-//		);
-//		box.getChildren().addAll(indeces, list);
-//		ColorPicker cp = new ColorPicker(Color.WHITE);
-//		MenuItem changeColor = new MenuItem();
-//		//changeColor.setGraphic(cp);
-//		changeColor.setGraphic(box);
-//		backgroundColor.getItems().add(changeColor);
-//		cp.setOnAction((event) -> {
-//			c.changeBackgroundAction(cp.getValue());
-//		}); 
-		
 		
 		Menu backgroundColor = new Menu(r.getString("backgroundTitle"));
-		String[] backgroundO = {"salmon", "green", "blue", "red", 
+		String[] backgroundOptions = {"salmon", "green", "blue", "red", 
 				"chocolate", "yellow", "pink", "purple", "orange"};
-		Rectangle[] rects = new Rectangle[backgroundO.length];
-		for (int i = 0; i < rects.length; i++) {
-			Rectangle rect = new Rectangle(50,10);
-			rect.setFill(Color.web(backgroundO[i]));
-			rects[i] = rect;
-		}
+		Rectangle[] backgroundColors = makeColorNodes(backgroundOptions);
 		String[] indices = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
-		addMenuItem(backgroundColor, indices, rects);
-			
+		addMenuItem(backgroundColor, indices, backgroundColors);
+		
+		
+		
 		Menu penColor = new Menu(r.getString("penTitle"));
-		String[] penO = {"salmon", "green", "blue", "red", 
+		String[] penOptions = {"salmon", "green", "blue", "red", 
 				"chocolate", "yellow", "pink", "purple", "orange"};
-		Rectangle[] penRects = new Rectangle[backgroundO.length];
-		for (int i = 0; i < rects.length; i++) {
-			Rectangle rect = new Rectangle(50,10);
-			rect.setFill(Color.web(penO[i]));
-			penRects[i] = rect;
-		}
+		Rectangle[] penColors = makeColorNodes(penOptions);
 		String[] penIndices = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
-		addMenuItem(penColor, penIndices, penRects);
+		addMenuItem(penColor, penIndices, penColors);
 		
 		
 		Menu image = new Menu(r.getString("imageTitle"));
-		addImageItems(image, new String[] {r.getString("imageItem1"), r.getString("imageItem2"), r.getString("imageItem3")}, 
-				c);
+		String[] imageOptions = {r.getString("imageItem1"), r.getString("imageItem2"), r.getString("imageItem3")};
+		Rectangle[] imageRects = makeImageNodes(imageOptions, 50, 50);
+		addMenuItem(image, imageOptions, imageRects);
+		
 		
 		Menu language = new Menu(r.getString("languageTitle"));
-		addLanguageItems(language, new String[] {r.getString("languageItem1"),
-						r.getString("languageItem2"),
-						r.getString("languageItem3"),
-						r.getString("languageItem4"),
-						r.getString("languageItem5"),
-						r.getString("languageItem6"),
-						r.getString("languageItem7"),
-						r.getString("languageItem8")}, c, rs);
+		String[] languageOptions = {r.getString("languageItem1"),
+				r.getString("languageItem2"),
+				r.getString("languageItem3"),
+				r.getString("languageItem4"),
+				r.getString("languageItem5"),
+				r.getString("languageItem6"),
+				r.getString("languageItem7"),
+				r.getString("languageItem8")};
+		String[] flagOptions = {r.getString("languageFlag1"),
+				r.getString("languageFlag2"),
+				r.getString("languageFlag3"),
+				r.getString("languageFlag4"),
+				r.getString("languageFlag5"),
+				r.getString("languageFlag6"),
+				r.getString("languageFlag7"),
+				r.getString("languageFlag8")};
+		Rectangle[] flagRects = makeImageNodes(flagOptions, 75, 50);
+		addMenuItem(language, languageOptions, flagRects);
 		
-		menuBar.getMenus().addAll(backgroundColor, image, penColor, language);
+		Menu help = new Menu(r.getString("helpTitle"));
+		String[] helpOptions = {r.getString("helpLink1"), r.getString("helpLink2")};
+		String[] helpImages = {r.getString("linkImage1"), r.getString("linkImage2")};
+		Rectangle[] helpRects = makeImageNodes(helpImages, 20, 20);
+		addMenuItem(help, helpOptions, helpRects);
+		
+		
+		
+		menuBar.getMenus().addAll(backgroundColor, image, penColor, language, help);
 		root.getChildren().add(menuBar);
 		
 		
