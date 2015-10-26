3fbaa78099d639398eb2c280d8386957d38ee2fe
Tyler Webner <tlw37@duke.edu>
2015-10-25 22:32:00 -0400

    fixed vertical angle wrap around

--- src/slogo_team03/LineSplitter.java
+++ src/slogo_team03/LineSplitter.java
@@ -2,15 +2,13 @@ package slogo_team03;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import javafx.geometry.Point2D;
-import javafx.scene.effect.Light.Point;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 
 public class LineSplitter {
-	private double m; // slope
-	private double b; // y-intercept
+	private double m;
+	private double b;
 	private double angle;
 	private Point2D pS, pE;
 
@@ -19,90 +17,80 @@ public class LineSplitter {
 		Color color = (Color) line.getFill();
 		double penSize = line.getStrokeWidth();
 
-
-//		double startX = line.getStartX();
-//		double startY = line.getStartX();
-//		double endX = line.getEndX();
-//		double endY = line.getEndY();
-		
-		
 		pS = new Point2D(line.getStartX(), line.getStartY());
 		pE = new Point2D(line.getEndX(), line.getEndY());
 		double length = distance(pS, pE);
-//		System.out.println("Length = " + length);
-		
 		m = (pE.getY() - pS.getY()) / (pE.getX() - pS.getX());
-		
-		if (pE.getX() > pS.getX()) {
-			angle = ((Math.toDegrees(Math.atan(m))) + 360) % 360;
-		} else { 
+		if (pE.getX() < pS.getX()) {
 			angle = ((Math.toDegrees(Math.atan(m))) + 180) % 360;
+		} else {
+			angle = ((Math.toDegrees(Math.atan(m))) + 360) % 360;
 		}
-		
-		
-		
-//		angle = ((Math.toDegrees(Math.atan(m))) + 360) % 360;
-		System.out.println("Angle = " + angle);
+
 		updateYIntercept(pS);
-//		System.out.println(angle);
 
 		double bound = 250.0;
 		while (pE.getX() > bound || pE.getY() > bound || pE.getX() < (bound * -1) || pE.getY() < (bound * -1)) {
-			
-			Point2D p1, p2;
-			if (angle > 0 && angle <= 90) {
+			if (angle == 90.0) {
+				while (pE.getY() > bound) {
+					Point2D p1 = new Point2D(pS.getX(), bound);
+					double dist = distance(pS, p1);
+					length -= dist;
+					lineList.add(new Line(pS.getX(), pS.getY(), p1.getX(), p1.getY()));
+					pS = new Point2D(p1.getX(), p1.getY() * -1);
+					pE = calculateNewEndPoint(pS, length);
+				}
 
-				p1 = new Point2D(bound, getYWhenXEquals(bound));		//x boundary
-				p2 = new Point2D(getXWhenYEquals(bound), bound);		//y boundary
-					
-			} else if (angle > 90 && angle <= 180) {
-				p1 = new Point2D(bound * -1, getYWhenXEquals(bound * -1));		//x boundary
-				p2 = new Point2D(getXWhenYEquals(bound), bound);		//y boundary
-			} else if (angle > 180 && angle <= 270) {
-				p1 = new Point2D(bound * -1, getYWhenXEquals(bound * -1));		//x boundary
-				p2 = new Point2D(getXWhenYEquals(bound * -1), bound * -1);		//y boundary
+			} else if (angle == 270.0) {
+				while (pE.getY() < bound * -1) {
+					Point2D p1 = new Point2D(pS.getX(), bound * -1);
+					double dist = distance(pS, p1);
+					length -= dist;
+					lineList.add(new Line(pS.getX(), pS.getY(), p1.getX(), p1.getY()));
+					pS = new Point2D(p1.getX(), p1.getY() * -1);
+					pE = calculateNewEndPoint(pS, length);
+				}
 			} else {
-				p1 = new Point2D(bound, getYWhenXEquals(bound));		//x boundary
-				p2 = new Point2D(getXWhenYEquals(bound * -1), bound * -1);		//y boundary
-			}
-			
+				Point2D p1, p2;
 
-//			System.out.println(p1);
-//			System.out.println(p2);
-			
-			
-			double d1 = distance(pS, p1);
-			double d2 = distance(pS, p2);
-//			System.out.println(d1);
-//			System.out.println(d2);
-			
-			if (d1 < d2){	//x		//d1 is smaller, pick this one
-				//Choose d1/p1
-				length -= d1;
-//				System.out.println("Length = " + length);
-				lineList.add(new Line(pS.getX(), pS.getY(), p1.getX(), p1.getY()));
-				pS = new Point2D(p1.getX() * -1, p1.getY());
-				pE = calculateNewEndPoint(pS, length);
-//				System.out.println(pS);
-//				System.out.println(pE);
+				if (angle > 0 && angle <= 90) {
+					p1 = new Point2D(bound, getYWhenXEquals(bound));
+					p2 = new Point2D(getXWhenYEquals(bound), bound);
+				} else if (angle > 90 && angle <= 180) {
+					p1 = new Point2D(bound * -1, getYWhenXEquals(bound * -1));
+					p2 = new Point2D(getXWhenYEquals(bound), bound);
+				} else if (angle > 180 && angle <= 270) {
+					p1 = new Point2D(bound * -1, getYWhenXEquals(bound * -1));
+					p2 = new Point2D(getXWhenYEquals(bound * -1), bound * -1);
+				} else {
+					p1 = new Point2D(bound, getYWhenXEquals(bound));
+					p2 = new Point2D(getXWhenYEquals(bound * -1), bound * -1);
+				}
+
+				double d1 = distance(pS, p1);
+				double d2 = distance(pS, p2);
+
+				if (d1 < d2) {
+					length -= d1;
+					lineList.add(new Line(pS.getX(), pS.getY(), p1.getX(), p1.getY()));
+					pS = new Point2D(p1.getX() * -1, p1.getY());
+					pE = calculateNewEndPoint(pS, length);
+				} else {
+					length -= d2;
+					lineList.add(new Line(pS.getX(), pS.getY(), p2.getX(), p2.getY()));
+					pS = new Point2D(p2.getX(), p2.getY() * -1);
+					pE = calculateNewEndPoint(pS, length);
+				}
+
+				updateYIntercept(pS);
+
 			}
-			else {	//y				//d2 is smaller, pick this one
-				//Choose d2/p2
-				length -= d2;
-				lineList.add(new Line(pS.getX(), pS.getY(), p2.getX(), p2.getY()));
-				pS = new Point2D(p2.getX(), p2.getY() * -1);
-				pE = calculateNewEndPoint(pS, length);
-			}
-			
-			updateYIntercept(pS);
 		}
-		
+
 		pE = calculateNewEndPoint(pS, length);
-//		System.out.println("Last line endpoint: " + pE);
 		lineList.add(new Line(pS.getX(), pS.getY(), pE.getX(), pE.getY()));
 
 		for (int i = 0; i < lineList.size(); i++) {
-			System.out.println("X0 = " + line.getStartX() + ", Y0 = " + line.getStartY() + ", X1 = " + line.getEndX() + ", Y1 = " + line.getEndY());
 			lineList.get(i).setFill(color);
 			lineList.get(i).setStrokeWidth(penSize);
 		}
@@ -111,25 +99,8 @@ public class LineSplitter {
 
 	private Point2D calculateNewEndPoint(Point2D pS, double length) {
 		double x1, y1;
-		
-//		System.out.println("Angle = " + angle);
-		//
-		// if (angle >= 90 && angle <= 270) {
-		// x1 = pS.getX() + length * Math.cos(Math.toRadians(angle));
-		// y1 = pS.getY() + length * Math.sin(Math.toRadians(angle));
-		//
-		// } else {
-		// x1 = pS.getX() + length * Math.cos(Math.toRadians(angle));
-		// y1 = pS.getY() + length * Math.sin(Math.toRadians(angle));
-		// }
-		//
-		// System.out.println(x1);
-		// System.out.println(y1);
-		//
-		
 		x1 = pS.getX() + length * Math.cos(Math.toRadians(angle));
 		y1 = pS.getY() + length * Math.sin(Math.toRadians(angle));
-
 		return new Point2D(x1, y1);
 	}
 
@@ -144,7 +115,7 @@ public class LineSplitter {
 	public double getXWhenYEquals(double yValue) {
 		return (yValue - b) / m;
 	}
-	
+
 	public double distance(Point2D p1, Point2D p2) {
 		double x1 = p1.getX();
 		double y1 = p1.getY();
